c4ba33da349cc164685df4dc1d3d33fbbf85bbd8
HONGBO LIU <liuhongbo@sysdyne.onmicrosoft.com>
2023-02-26 11:50:29 -0500

    initial commit

---
+++ QuickClientExample/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Net.Quic;
+using System.Net.Security;
+using System.Runtime.Versioning;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+#pragma warning disable CA1416
+
+namespace QuickClientExample
+{
+    [RequiresPreviewFeatures]
+    class Program
+    {
+        static async Task Main(string[] args)
+        {            
+
+            if (!QuicConnection.IsSupported)
+            {
+                Console.WriteLine("QUIC is not supported.");
+                return;
+            }
+
+            var protocol = "quic-test";
+            var port = 8000;
+
+            var clientConnectionOptions = new QuicClientConnectionOptions()
+            {
+                RemoteEndPoint = new IPEndPoint(IPAddress.Loopback, port),
+                DefaultStreamErrorCode = 0x0A,
+                DefaultCloseErrorCode = 0x0B,
+                MaxInboundUnidirectionalStreams = 10,
+                MaxInboundBidirectionalStreams = 100,
+                ClientAuthenticationOptions = new SslClientAuthenticationOptions()
+                {
+                    ApplicationProtocols = new List<SslApplicationProtocol>() { new SslApplicationProtocol(protocol) }
+                }
+            };
+            var connection = await QuicConnection.ConnectAsync(clientConnectionOptions);
+            var outgoingStream = await connection.OpenOutboundStreamAsync(QuicStreamType.Bidirectional);
+
+            var str = "hello world";
+            var buf = Encoding.UTF8.GetBytes(str);
+            await outgoingStream.WriteAsync(buf, 0, buf.Length);
+
+            Console.ReadKey();
+
+            await connection.CloseAsync(0x0C);
+            await connection.DisposeAsync();
+        }
+    }
+}
---
+++ QuickServerExample/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net;
+using System.Net.Quic;
+using System.Net.Security;
+using System.Runtime.Versioning;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+#pragma warning disable CA1416
+
+namespace QuickClientExample
+{
+    [RequiresPreviewFeatures]
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var isRunning = true;
+
+            if (!QuicListener.IsSupported)
+            {
+                Console.WriteLine("QUIC is not supported.");
+                return;
+            }
+            var protocol = "quic-test";
+            var pfxFilePath = "your pfx file path";
+            var pfxPassword = "your pfx file password";
+            var port = 8000;
+
+            var serverConnectionOptions = new QuicServerConnectionOptions()
+            {
+                DefaultStreamErrorCode = 0x0A,
+                DefaultCloseErrorCode = 0x0B,                         
+                ServerAuthenticationOptions = new SslServerAuthenticationOptions
+                {
+                    ApplicationProtocols = new List<SslApplicationProtocol>() { new SslApplicationProtocol(protocol) },                    
+                    ServerCertificate = new X509Certificate2(pfxFilePath, pfxPassword)
+                }
+            };            
+
+            var listener = await QuicListener.ListenAsync(new QuicListenerOptions()
+            {
+                ListenEndPoint = new IPEndPoint(IPAddress.Loopback, port),
+                ApplicationProtocols = new List<SslApplicationProtocol>() { new SslApplicationProtocol(protocol) },
+                ConnectionOptionsCallback = (_, _, _) => ValueTask.FromResult(serverConnectionOptions)
+            });           
+
+
+            while (isRunning)
+            {
+                var connection = await listener.AcceptConnectionAsync();
+                var stream = await connection.AcceptInboundStreamAsync();
+                
+                // Work with the incoming stream ...
+                var buf = new byte[128];
+                var len = await stream.ReadAsync(buf, 0, buf.Length);
+                Console.WriteLine(Encoding.UTF8.GetString(buf, 0, len));                
+            }
+
+
+            await listener.DisposeAsync();
+        }
+    }
+}
